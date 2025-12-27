3b25f5f6a3fd22588a392b056a23404ffea8167e
fayarsh <faydarsh2000@gmail.com>
2025-12-27 22:55:38 +0530

    fonts

---
+++ public/GradientDiv.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const GradientDiv = () => {
+  return (
+    <div className='w-full absolute -bottom-10 h-27.5 bg-white/10 blur-xl'>
+      
+    </div>
+  )
+}
+
+export default GradientDiv
